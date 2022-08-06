36f9ceb9e168befe7f349d902d8808b51bb69b1e
ktoqaloglu <sector3hd@gmail.com>
2022-08-06 17:06:07 +0300

    test automation prepared

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import Header from "./Header.js";
+import App from "./App.js";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe("patika.dev Test ", () => {
+  test("Header bölümü başarılı bir şekilde yüklendi mi?", () => {
+    render(<Header />);
+    const head = screen.getByText("Emoji Search");
+    expect(head).toBeInTheDocument();
+  });
+  test("Emoji listesi başarılı bir şekilde yüklendi mi?", () => {
+    render(<App />);
+    const items = screen.getAllByText(/Click to copy emoji/i);
+    const item = screen.getByText("Relaxed");
+    expect(item).toBeInTheDocument();
+    expect(items.length).toEqual(20);
+  });
+  test("Filtreleme Başarılı mı? ", () => {
+    render(<App />);
+    let input = screen.getByPlaceholderText("Search...");
+    userEvent.type(input, "wink");
+    let item = screen.getByText("Wink");
+    expect(item).toBeInTheDocument();
+  });
+  test("Tıklama sonucunda emoji kopyalandı mı? ", () => {
+    render(<App />);
+    const clicks = screen.getAllByTestId("row");
+    expect(clicks[0]).toHaveAttribute("data-clipboard-text");
+  });
 });
